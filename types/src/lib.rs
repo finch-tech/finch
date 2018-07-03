3550889b8ba63ad175cc780f7d066e0d3add9311
Shotaro Suganuma <shotaro@artefact.co.jp>
2018-07-03 22:51:41 +0900

    Add ethereum transaction type.

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -8,24 +8,31 @@ extern crate num_traits;
 extern crate ripemd160;
 extern crate rustc_hex;
 extern crate serde;
-extern crate sha2;
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+extern crate sha2;
 extern crate web3;
 
+mod block;
 mod clients;
 mod currencies;
 mod h160;
 mod h256;
 mod payment_status;
+mod transaction;
 mod u256;
 
 pub type PrivateKey = Vec<u8>;
 pub type PublicKey = Vec<u8>;
 
+pub use self::block::Block;
+pub use self::block::BlockHeader;
 pub use self::clients::Client;
 pub use self::currencies::Currency;
 pub use self::h160::H160;
 pub use self::h256::H256;
 pub use self::payment_status::Status;
+pub use self::transaction::Transaction;
 pub use self::u256::U256;
