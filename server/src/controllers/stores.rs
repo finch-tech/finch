323a8ce180a6aeb5942b0289b5739a490c3a14ac
Shotaro Suganuma <shotaro@artefact.co.jp>
2018-09-13 15:53:39 +0900

    Add store owner validation to stores endpoints.

--- server/src/controllers/stores.rs
+++ server/src/controllers/stores.rs
@@ -1,10 +1,10 @@
 use actix_web::{Json, Path, State};
-use futures::future::Future;
+use futures::future::{Future, IntoFuture};
 use serde_json::Value;
 use uuid::Uuid;
 
 use auth::AuthUser;
-use core::store::StorePayload;
+use core::store::{Store, StorePayload};
 use currency_api_client::Api as CurrencyApi;
 use server::AppState;
 use services::{self, Error};
@@ -59,32 +59,46 @@ pub struct PatchParams {
     pub currency_api_key: Option<String>,
 }
 
+fn validate_store_owner(store: &Store, user: &AuthUser) -> Result<bool, Error> {
+    if store.owner_id != user.id {
+        return Err(Error::InvalidRequestAccount);
+    }
+
+    Ok(true)
+}
+
 pub fn patch(
     (state, path, params, user): (State<AppState>, Path<Uuid>, Json<PatchParams>, AuthUser),
 ) -> impl Future<Item = Json<Value>, Error = Error> {
     let id = path.into_inner();
     let params = params.into_inner();
 
-    let payload = StorePayload {
-        id: None,
-        name: params.name,
-        description: params.description,
-        owner_id: user.id,
-        private_key: None,
-        public_key: None,
-        created_at: None,
-        updated_at: None,
-        payout_addresses: params.payout_addresses,
-        mnemonic: None,
-        hd_path: None,
-        base_currency: params.base_currency,
-        currency_api: params.currency_api,
-        currency_api_key: params.currency_api_key,
-        active: true,
-    };
+    services::stores::get(id, &state.postgres).and_then(move |store| {
+        validate_store_owner(&store, &user)
+            .into_future()
+            .and_then(move |_| {
+                let payload = StorePayload {
+                    id: None,
+                    name: params.name,
+                    description: params.description,
+                    owner_id: user.id,
+                    private_key: None,
+                    public_key: None,
+                    created_at: None,
+                    updated_at: None,
+                    payout_addresses: params.payout_addresses,
+                    mnemonic: None,
+                    hd_path: None,
+                    base_currency: params.base_currency,
+                    currency_api: params.currency_api,
+                    currency_api_key: params.currency_api_key,
+                    active: true,
+                };
 
-    services::stores::patch(id, payload, &state.postgres)
-        .then(|res| res.and_then(|store| Ok(Json(store.export()))))
+                services::stores::patch(id, payload, &state.postgres)
+                    .then(|res| res.and_then(|store| Ok(Json(store.export()))))
+            })
+    })
 }
 
 pub fn list(
@@ -102,19 +116,31 @@ pub fn list(
 }
 
 pub fn get(
-    (state, path, _): (State<AppState>, Path<Uuid>, AuthUser),
+    (state, path, user): (State<AppState>, Path<Uuid>, AuthUser),
 ) -> impl Future<Item = Json<Value>, Error = Error> {
     let id = path.into_inner();
 
-    services::stores::get(id, &state.postgres)
-        .then(|res| res.and_then(|store| Ok(Json(store.export()))))
+    services::stores::get(id, &state.postgres).and_then(move |store| {
+        validate_store_owner(&store, &user)
+            .into_future()
+            .and_then(move |_| {
+                services::stores::get(id, &state.postgres)
+                    .then(|res| res.and_then(|store| Ok(Json(store.export()))))
+            })
+    })
 }
 
 pub fn delete(
-    (state, path, _): (State<AppState>, Path<Uuid>, AuthUser),
+    (state, path, user): (State<AppState>, Path<Uuid>, AuthUser),
 ) -> impl Future<Item = Json<Value>, Error = Error> {
     let id = path.into_inner();
 
-    services::stores::delete(id, &state.postgres)
-        .then(|res| res.and_then(|res| Ok(Json(json!({ "deleted": res })))))
+    services::stores::get(id, &state.postgres).and_then(move |store| {
+        validate_store_owner(&store, &user)
+            .into_future()
+            .and_then(move |_| {
+                services::stores::delete(id, &state.postgres)
+                    .then(|res| res.and_then(|res| Ok(Json(json!({ "deleted": res })))))
+            })
+    })
 }
