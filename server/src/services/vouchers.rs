c6573e153b2e55c79d7758af1a60f23b4b0cbf60
Shotaro Suganuma <shotaro@artefact.co.jp>
2018-09-18 15:59:14 +0900

    Check block height required when issuing voucher.

--- server/src/services/vouchers.rs
+++ server/src/services/vouchers.rs
@@ -1,5 +1,6 @@
-use futures::future::{err, Future, IntoFuture};
+use futures::future::{err, ok, Future, IntoFuture};
 
+use core::app_status::AppStatus;
 use core::db::postgres::PgExecutorAddr;
 use core::payment::Payment;
 use core::voucher::Voucher;
@@ -14,16 +15,37 @@ pub fn create(
 
     let transaction = payment.transaction(&postgres).from_err();
     let store = payment.store(&postgres).from_err();
+    let status = AppStatus::find(&postgres).from_err();
 
-    match payment.status {
-        PaymentStatus::Paid => Box::new(transaction.join(store).and_then(
-            move |(transaction, store)| {
+    Box::new(
+        transaction
+            .join(store)
+            .join(status)
+            .and_then(move |((transaction, store), status)| {
+                if let None = status.block_height {
+                    return err(Error::PaymentNotConfirmed);
+                }
+
+                if let None = payment.eth_block_height_required {
+                    return err(Error::PaymentNotConfirmed);
+                }
+
+                if status.block_height.unwrap().0
+                    < payment.eth_block_height_required.clone().unwrap().0
+                {
+                    return err(Error::PaymentNotConfirmed);
+                }
+
+                match payment.status {
+                    PaymentStatus::Paid => ok((payment, transaction, store)),
+                    _ => err(Error::PaymentNotConfirmed),
+                }
+            })
+            .and_then(move |(payment, transaction, store)| {
                 Voucher::new(payment, transaction)
                     .encode(&store.private_key)
                     .into_future()
                     .from_err()
-            },
-        )),
-        _ => Box::new(err(Error::PaymentNotConfirmed)),
-    }
+            }),
+    )
 }
