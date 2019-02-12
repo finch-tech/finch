8d217063a57e5e9025bad9923eb21439ea1c603c
Shotaro Suganuma <shotaro@artefact.co.jp>
2019-02-12 23:18:45 +0900

    Modify project name on the root endpoint.

--- server/src/controllers/root.rs
+++ server/src/controllers/root.rs
@@ -5,7 +5,7 @@ use state::AppState;
 
 pub fn index(_: (HttpRequest<AppState>)) -> FutureResponse<HttpResponse> {
     ok(HttpResponse::Ok().json(json!({
-        "name": "Finch Payment Gateway Server",
+        "name": "Finch Cryptocurrency Payment Processor",
         "version": "0.1.0"
     })))
     .responder()
